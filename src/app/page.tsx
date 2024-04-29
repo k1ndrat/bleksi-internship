498ad3c4a8abd3907548c9bd35388faa1d52eda3
k1ndrat <rubalkanaspining@gmail.com>
2024-04-29 21:29:50 +0300

    PAGES | Main, Task-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-yellow"></main>
+    <main className="flex min-h-screen flex-col items-center gap-5 p-24">
+      <Link href={"/task-1"} className="text-4xl ">
+        Task 1
+      </Link>
+    </main>
   );
 }
---
+++ src/app/task-1/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Task1() {
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-5 p-24">
+      <Link href={"/task-1/radix-ui"} className="text-4xl ">
+        Radix-UI
+      </Link>
+      <Link href={"/task-1/shadcn-ui"} className="text-4xl ">
+        Shadcn-UI
+      </Link>
+    </main>
+  );
+}
